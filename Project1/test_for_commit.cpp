a8988c411ce7c34779bd0fd328ce7fb19caa6ab2
useok123 <useok4229@gmail.com>
2025-03-11 10:50:38 +0900

    test-commit

--- Project1/test_for_commit.cpp
+++ Project1/test_for_commit.cpp
@@ -1,5 +1,17 @@
 #include<stdio.h>
 
 void main(void) {
-	printf("First Commit!");
+	unsigned int input = 0;
+	printf("정수를 입력하세요: ");
+	scanf_s("%u", &input);
+	int result = input;
+	printf("입력된 정수의 32비트 표현: ");
+	if (input == 0) {
+	}
+	else {
+		for (int i = 31; i >= 0; i--) {
+			printf("%d", (input >> i) & 1);
+		}
+	}
+
 }
