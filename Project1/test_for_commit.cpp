289d446e0a0710ca3b79a5b19e2197715823a7d8
useok123 <useok4229@gmail.com>
2025-03-11 12:13:10 +0900

    test-commit

--- Project1/test_for_commit.cpp
+++ Project1/test_for_commit.cpp
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
 void main(void) {
-	unsigned int input = 0;
-	printf("정수를 입력하세요: ");
-	scanf_s("%u", &input);
-	int result = input;
-	printf("입력된 정수의 32비트 표현: ");
+	unsigned int input = 0;	//변수 초기화
+	printf("정수를 입력하세요: ");	//문자열 출력
+	scanf_s("%u", &input);	//임의의 정수 입력
+	int result = input;	//result 변수에 값 대입
+	printf("입력된 정수의 32비트 표현: ");	//문자열 출력
 	if (input == 0) {
 	}
-	else {
-		for (int i = 31; i >= 0; i--) {
-			printf("%d", (input >> i) & 1);
+	else {	//입력값이 0이 아닐때 
+		for (int i = 31; i >= 0; i--) {	//i가 0보다 작아질때까지 반복함
+			printf("%d", (input >> i) & 1);	//32비트를 출력한다
 		}
 	}
 
